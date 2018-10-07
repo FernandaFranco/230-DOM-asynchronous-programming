4fe4fc448a4572754b3000bf8e448546eb8150a4
Fernanda Franco <francodias.fernanda@gmail.com>
2018-10-06 23:40:19 -0400

    end of lesson 1

--- notes.md
+++ notes.md
@@ -1,5 +1,7 @@
 * DOM
 
+  * Object representation of an HTML document. It provides a way to interact with a web page using JavaScript.
+
   * JavaScript is the syntax, the DOM is where stuff goes down. A DOM node can be a DOM element, a text node, a comment, or other types. DOM nodes have properties.
 
   * Browsers insert missing elements like `HEAD` and `BODY` in the DOM even if the corresponding tags don't appear in our HTML, since a fundamental tenet of the web is permissiveness; they always do their best to display HTML even when it has errors.
--- practice_problems.js
+++ practice_problems.js
@@ -19,9 +19,36 @@ function walk(node, callback) {
 //   });
 // });
 
-var introParagraphs = document.querySelectorAll('.intro p');
-introParagraphs = [].slice.call(introParagraphs);
-introParagraphs.forEach(function(paragraph) {
-  paragraph.classList.add('article-text');
-  console.log(paragraph);
-});
+// var introParagraphs = document.querySelectorAll('.intro p');
+// introParagraphs = [].slice.call(introParagraphs);
+// introParagraphs.forEach(function(paragraph) {
+//   paragraph.classList.add('article-text');
+//   console.log(paragraph);
+// });
+
+
+// var notice = document.getElementById('notice');
+// document.getElementById('toggle').onclick = function(e) {
+//   e.preventDefault();
+//   if (notice.getAttribute('class') === 'hidden') {
+//     notice.setAttribute('class', 'visible');
+//   } else {
+//     notice.setAttribute('class', 'hidden');
+//   }
+// };
+
+// notice.onclick = function(e) {
+//   e.preventDefault();
+//   this.setAttribute('class', 'hidden');
+// };
+
+// var multi = document.getElementById('multiplication');
+// multi.innerText = String(13*9);
+var addIntroInfo = document.querySelector('.intro').cloneNode(true);
+var addIntroInfoLink = addIntroInfo.querySelector('a');
+
+addIntroInfo.firstChild.nodeValue = 'We also have previous new letters.';
+addIntroInfoLink.textContent = 'Check them out here.';
+addIntroInfoLink.href = 'newsletters.html';
+
+document.body.insertBefore(addIntroInfo, document.querySelector('.form'));
