b37013b9924898336a4b476d13f1464cc50a1374
Fernanda Franco <francodias.fernanda@gmail.com>
2018-10-07 23:12:49 -0400

    exercises

--- practice_problems.js
+++ practice_problems.js
@@ -7,48 +7,24 @@ function walk(node, callback) {
   }
 }
 
+function domTreeTracer(id) {
+  var elem = document.getElementById(id);
+  var result = [];
+  while (elem) {
+    result.push(siblingsOf(elem));
+    elem = elem.parentElement;
+  }
 
-// var intros = document.getElementsByClassName('intro')
-// intros = [].slice.call(intros);
-// var paragraphs;
-// intros.forEach(function(div) {
-//   paragraphs = [].slice.call(div.getElementsByTagName('P'));
-//   paragraphs.forEach(function(paragraph) {
-//     paragraph.classList.add('article-text');
-//     console.log(paragraph);
-//   });
-// });
-
-// var introParagraphs = document.querySelectorAll('.intro p');
-// introParagraphs = [].slice.call(introParagraphs);
-// introParagraphs.forEach(function(paragraph) {
-//   paragraph.classList.add('article-text');
-//   console.log(paragraph);
-// });
+  return result;
+}
 
 
-// var notice = document.getElementById('notice');
-// document.getElementById('toggle').onclick = function(e) {
-//   e.preventDefault();
-//   if (notice.getAttribute('class') === 'hidden') {
-//     notice.setAttribute('class', 'visible');
-//   } else {
-//     notice.setAttribute('class', 'hidden');
-//   }
-// };
+function siblingsOf(elem) {
+  var sibilings = [];
+  while (elem) {
+    sibilings.push(elem);
+    elem = elem.nextElementSibling;
+  }
 
-// notice.onclick = function(e) {
-//   e.preventDefault();
-//   this.setAttribute('class', 'hidden');
-// };
-
-// var multi = document.getElementById('multiplication');
-// multi.innerText = String(13*9);
-var addIntroInfo = document.querySelector('.intro').cloneNode(true);
-var addIntroInfoLink = addIntroInfo.querySelector('a');
-
-addIntroInfo.firstChild.nodeValue = 'We also have previous new letters.';
-addIntroInfoLink.textContent = 'Check them out here.';
-addIntroInfoLink.href = 'newsletters.html';
-
-document.body.insertBefore(addIntroInfo, document.querySelector('.form'));
+  return sibilings;
+}
