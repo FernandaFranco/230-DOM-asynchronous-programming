91325f7a2b255465dcda9632cfa2933474b826a9
Fernanda Franco <francodias.fernanda@gmail.com>
2018-10-09 17:34:27 -0400

    done with first load of exercises

--- practice_problems.js
+++ practice_problems.js
@@ -1,17 +1,17 @@
-var currentParent = document.body;
+var id;
 
-function nodesToArr() {
-  var currentChildren = [].slice.call(currentParent.children);
-  var result = [currentParent.tagName, currentChildren];
-  var i;
-
-  for (i = 0; i < currentChildren.length; i += 1) {
-    currentParent = currentChildren[i];
-    currentChildren[i] = nodesToArr();
-  }
-
-  return result;
+function startCounting() {
+  var n = 1;
+  id = setInterval(function() {
+    console.log(n);
+    n += 1;
+  }, 1000);
 }
 
+// startCounting();
 
-nodesToArr();
+function stopCounting() {
+  clearInterval(id);
+}
+
+// stopCounting();
