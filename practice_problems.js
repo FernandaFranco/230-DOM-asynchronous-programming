1c3ff8f84b4ccdfb08298deeff13102b4c02ae9d
Fernanda Franco <francodias.fernanda@gmail.com>
2018-10-16 19:32:56 -0400

    started events and async programming exercises

--- practice_problems.js
+++ practice_problems.js
@@ -1,17 +1,41 @@
-var id;
+function callback1() {
+  console.log('callback1');
+}
 
-function startCounting() {
-  var n = 1;
-  id = setInterval(function() {
-    console.log(n);
-    n += 1;
+function callback2() {
+  console.log('callback2');
+}
+
+function callback3() {
+  console.log('callback3');
+}
+
+function randomizer(callbacks) {
+  callbacks = [].slice.call(arguments);
+  var maxSeconds = 2 * callbacks.length;
+  var counter = 0;
+  var timer = setInterval(function() {
+    counter += 1;
+    console.log(counter);
+
+    if (counter >= maxSeconds) {
+      clearInterval(timer);
+    }
   }, 1000);
+
+  callbacks.forEach(function(callback) {
+    var pointInTime = Math.floor(Math.random() * (maxSeconds * 1000 + 1));
+    setTimeout(callback, pointInTime);
+  });
 }
-
-// startCounting();
-
-function stopCounting() {
-  clearInterval(id);
-}
-
-// stopCounting();
+randomizer(callback1, callback2, callback3);
+// Output:
+// 1
+// 2
+// "callback2"
+// "callback3"
+// 3
+// 4
+// 5
+// "callback1"
+// 6
