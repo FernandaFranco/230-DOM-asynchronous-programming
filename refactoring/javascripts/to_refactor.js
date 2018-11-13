546173557a6920323924f0d381743fa5fa2c4733
Fernanda Franco <francodias.fernanda@gmail.com>
2018-11-13 18:20:00 -0500

    assignment refactoring completed

--- refactoring/javascripts/to_refactor.js
+++ refactoring/javascripts/to_refactor.js
@@ -1,16 +1,16 @@
 $(function() {
   $("nav a").on("mouseenter", function() {
-    $(this).next("ul").addClass('opened');
+    $(this).next("ul").addClass('visible');
   });
 
   $("nav").on("mouseleave", function() {
-    $(this).find("ul ul").removeClass('opened');
+    $(this).find("ul ul").removeClass('visible');
   });
 
-  $("main h2 + p").on("click", function(e) {
+  $(".button, button").on("click", function(e) {
     e.preventDefault();
 
-    $(e.target).addClass("clicked");
+    $(this).addClass("clicked");
   });
 
   $(".toggle").on("click", function(e) {
@@ -19,10 +19,50 @@ $(function() {
     $(this).next(".accordion").toggleClass("opened");
   });
 
-  function isValidCC(this) {
-    var cc_number = $(this).find("[type=text]").val(),
-        odd_total = 0,
-        even_total = 0;
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    var cc_number = $(this).find("[type=text]").val();
+    var total;
+    var isValid;
+
+    if (cc_number.length < 1) {
+      isValid = false;
+    } else {
+      total = getLuhnTotal(cc_number);
+      isValid = total % 10 == 0;
+    }
+
+    $(this).find(".success").toggle(isValid);
+    $(this).find(".error").toggle(!isValid);
+
+  });
+
+  $("ul a").on("click", function(e) {
+    e.preventDefault();
+
+    var birthstones = {
+      "January": "garnet",
+      "February": "amethyst",
+      "March": "aquamarine or bloodstone",
+      "April": "diamond",
+      "May": "emerald",
+      "June": "pearl, moonstone, or alexandrite",
+      "July": "ruby",
+      "August": "peridot",
+      "September": "sapphire",
+      "October": "opal or tourmaline",
+      "November": "topaz or citrine",
+      "December": "turquoise, zircon, or tanzanite",
+    };
+    var month = $(this).text();
+    var $stone = $("#birthstone");
+
+    $stone.text('Your birthstone is ' + birthstones[month]);
+  });
+
+  function getLuhnTotal(cc_number) {
+    var odd_total = 0;
+    var even_total = 0;
 
     cc_number = cc_number.split("").reverse();
     for (var i = 0, len = cc_number.length; i < len; i++) {
@@ -41,63 +81,6 @@ $(function() {
       }
     }
 
-    return (odd_total + even_total) % 10 == 0;
+    return odd_total + even_total
   }
-  $("form").on("submit", function(e) {
-    e.preventDefault();
-    if (isValidCC(this)) {
-      $(this).find(".success").show();
-      $(this).find(".error").hide();
-
-      $(this).find(".error").show();
-      $(this).find(".success").hide();
-    }
-
-  });
-
-  $("ul a").on("click", function(e) {
-    e.preventDefault();
-
-    var month = $(this).text(),
-        $stone = $("#birthstone");
-
-    switch (month) {
-      case "January":
-        $stone.text("Your birthstone is garnet");
-        break;
-      case "February":
-        $stone.text("Your birthstone is amethyst");
-        break;
-      case "March":
-        $stone.text("Your birthstone is aquamarine or bloodstone");
-        break;
-      case "April":
-        $stone.text("Your birthstone is diamond");
-        break;
-      case "May":
-        $stone.text("Your birthstone is emerald");
-        break;
-      case "June":
-        $stone.text("Your birthstone is pearl, moonstone, or alexandrite");
-        break;
-      case "July":
-        $stone.text("Your birthstone is ruby");
-        break;
-      case "August":
-        $stone.text("Your birthstone is peridot");
-        break;
-      case "September":
-        $stone.text("Your birthstone is sapphire");
-        break;
-      case "October":
-        $stone.text("Your birthstone is opal or tourmaline");
-        break;
-      case "November":
-        $stone.text("Your birthstone is topaz or citrine");
-        break;
-      case "December":
-        $stone.text("Your birthstone is turquoise, zircon, or tanzanite");
-        break;
-    }
-  });
 });
