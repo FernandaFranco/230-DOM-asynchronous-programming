fb8cb5536ada9f2e71d7fccc7d195301e2f0ac81
Fernanda Franco <francodias.fernanda@gmail.com>
2018-11-14 14:38:58 -0500

    animations on

--- comic/comic.js
+++ comic/comic.js
@@ -1,7 +1,16 @@
 $(function() {
   var $blinds = $('[id^=blind');
+  var speed = 250;
+  var delay = 1500;
+  var totalDelay;
+
   $blinds.each(function(i) {
     var $blind = $blinds.eq(i);
-    $blind.slideUp(250).delay(i * 1500);
+    totalDelay = (delay * i) + speed;
+
+    $blind.delay(totalDelay).animate({
+      top: '+=height',
+      height: '0',
+    }, speed);
   });
 });
