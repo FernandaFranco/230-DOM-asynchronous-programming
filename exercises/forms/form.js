4397bb049d736ee54a172fce9ae061d4e607b1a9
Fernanda Franco <francodias.fernanda@gmail.com>
2018-12-17 18:18:23 -0500

    form validatation: missing refactoration

--- exercises/forms/form.js
+++ exercises/forms/form.js
@@ -1,12 +1,41 @@
 $('dl').on('blur', 'input', function(e) {
-  e.preventDefault();
-  if ($(this).attr) {}
-  $(this).addClass('invalid');
-  console.log($(this));
+  var $label = $(this).closest('dd').prev('dt').find('label');
+  if ($(e.target)[0].validity.valueMissing) {
+    throwErrorMessage.call(this, $label.html() + ' is a required field')
+  } else if (this.validity.patternMismatch) {
+    throwErrorMessage.call(this, 'Please Enter a valid ' + $label.html() + '.');
+    if (this.id === 'password') {
+      throwErrorMessage.call(this, 'Password must be at least 10 characters long.');
+    }
+  }
 });
 
+function throwErrorMessage(message) {
+  $(this).addClass('invalid');
+  $(this).siblings('.error').html(message);
+}
+
 $('dl').on('focus', 'input', function(e) {
+  $(this).removeClass('invalid');
+  $(this).siblings('.error').html('');
+});
+
+$('form').on('submit', function(e) {
   e.preventDefault();
-  $(this).addClass('focus');
-  console.log('heheehe');
+  if ($('form')[0].checkValidity()) {
+    $('fieldset > p').html('');
+  } else {
+    $('fieldset > p').html('Form cannot be submitted until errors are corrected.');
+    $('input').each(function(_, input) {
+      var $label = $(input).closest('dd').prev('dt').find('label');
+      if (input.validity.valueMissing) {
+        throwErrorMessage.call(input, $label.html() + ' is a required field');
+      } else if (input.validity.patternMismatch) {
+        throwErrorMessage.call(input, 'Please Enter a valid ' + $label.html() + '.');
+        if (input.id === 'password') {
+          throwErrorMessage.call(input, 'Password must be at least 10 characters long.');
+        }
+      }
+    });
+  }
 });
