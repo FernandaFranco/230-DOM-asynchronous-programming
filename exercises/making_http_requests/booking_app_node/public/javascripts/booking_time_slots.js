21b0620ab260b3aa983c006761606c27eb7c23a2
Fernanda Franco <francodias.fernanda@gmail.com>
2019-02-13 13:25:36 -0500

    more booking app

---
+++ exercises/making_http_requests/booking_app_node/public/javascripts/booking_time_slots.js
@@ -0,0 +1,19 @@
+/*
+- make a post request to /api/bookings
+- data in json format to send: id (of the schedule) and student_email
+- but first, make a get request to /api/schedules to retrieve all schedules
+- filter out taken slots
+- display available schedules as :staff_name | :date | :time
+- keep track of schedule id
+- if student doesnt exist in the database, show box for student sign up
+- create new booking sequence for student sign up
+- after successfully signing up, add student to database (post /api/students) and
+create booking
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  form.addEventListener('submit', () => {
+    let request = new XMLHttpRequest();
+  });
+});
