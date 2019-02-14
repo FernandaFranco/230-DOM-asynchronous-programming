55f6d25fd4e745a5ae83c80b407057d71b0cd1ac
Fernanda Franco <francodias.fernanda@gmail.com>
2019-02-14 16:09:14 -0500

    booking time slots

--- exercises/making_http_requests/booking_app_node/public/javascripts/booking_time_slots.js
+++ exercises/making_http_requests/booking_app_node/public/javascripts/booking_time_slots.js
@@ -12,8 +12,115 @@ create booking
 */
 
 document.addEventListener('DOMContentLoaded', () => {
+  let bookingForm = document.querySelector('#booking_form');
+  let staffMembers = [];
 
-  form.addEventListener('submit', () => {
+  (() => {
     let request = new XMLHttpRequest();
+    request.open('GET', '/api/staff_members');
+    request.responseType = 'json'
+
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        staffMembers = request.response;
+      }
+    });
+
+    request.send();
+  })();
+
+  (() => {
+    let request = new XMLHttpRequest();
+    request.open('GET', '/api/schedules');
+    request.responseType = 'json'
+
+    request.addEventListener('load', () => {
+      let freeSchedules = request.response.filter((schedule) => {
+        return !schedule.student_email;
+      });
+
+      freeSchedules.forEach((schedule) => {
+        let bookingSlots = bookingForm.querySelector('select');
+        let option = document.createElement('option');
+        let staffMember = staffMembers.filter((staffMember) => {
+          return schedule.staff_id === staffMember.id;
+        })[0];
+        option.value = schedule.id;
+        option.textContent = `${staffMember.name} | ${schedule.date} | ${schedule.time}`;
+        bookingSlots.appendChild(option);
+      });
+    });
+
+    request.send();
+  })();
+
+  bookingForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data =
+    {
+      id: parseInt(bookingForm.querySelector('select').value, 10),
+      student_email: bookingForm.querySelector('#email').value,
+    };
+
+    let request = new XMLHttpRequest();
+    request.open('POST', bookingForm.action);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.addEventListener('load', () => {
+      if (request.status === 204) {
+        alert('Booked');
+        bookingForm.reset();
+      } else if (request.status === 404) {
+        alert(request.response);
+        if (request.response !== 'Schedule is either booked or does not exist.') {
+          let bookingSequence = parseInt(request.response.split(' ').pop());
+          createNewStudent(bookingSequence, data.student_email);
+        }
+      }
+    });
+
+    request.send(JSON.stringify(data));
   });
+
+  let createNewStudent = (bookingSequence, email) => {
+    let newStudentForm = document.createElement('form');
+    let newStudentName = document.createElement('input');
+    let newStudentEmail = document.createElement('input');
+    let newStudentBookingSequence = document.createElement('input');
+    let newFormSubmitButton = document.createElement('input');
+    newStudentForm.setAttribute('action', '/api/students');
+    newStudentEmail.value = email;
+    newStudentBookingSequence.value = bookingSequence;
+    newFormSubmitButton.setAttribute('type', 'submit');
+    newStudentForm.appendChild(newStudentName);
+    newStudentForm.appendChild(newStudentEmail);
+    newStudentForm.appendChild(newStudentBookingSequence);
+    newStudentForm.appendChild(newFormSubmitButton);
+    document.body.appendChild(newStudentForm);
+
+
+    newStudentForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let request = new XMLHttpRequest();
+      let data = {
+        email: email,
+        booking_sequence: bookingSequence,
+        name: newStudentName.value,
+      }
+      request.open('POST', newStudentForm.action);
+      request.setRequestHeader('Content-Type', 'application/json');
+
+      request.addEventListener('load', (e) => {
+        alert(request.response);
+        if (request.status === 201) {
+          let event = new Event('submit');
+          bookingForm.dispatchEvent(event);
+          newStudentForm.remove();
+        }
+      });
+
+      request.send(JSON.stringify(data));
+    });
+  };
 });
