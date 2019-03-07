8d192eb5aa7d8f43437d8d6f0bc0d17c26c68e55
Fernanda Franco <francodias.fernanda@gmail.com>
2019-03-07 15:23:56 -0500

    animation

--- contact_manager_sinatra/public/javascripts/app.js
+++ contact_manager_sinatra/public/javascripts/app.js
@@ -1,20 +1,32 @@
 const App = {
-  handleContactCreation(e) {
+  ANIMATION_DELAY: 300,
+  tags: ['friends', 'work', 'school'],
+  handleForm(e) {
     e.preventDefault();
-    $('#home_container').slideUp(500);
-    $('#create_contact').slideDown(500);
+
+    // $('#home_container').slideToggle(this.ANIMATION_DELAY);
+    $('#create_contact').slideToggle(this.ANIMATION_DELAY);
+  },
+  handleTagging(e) {
+    const template = Handlebars.compile($('#tags_template').html());
+    Handlebars.registerPartial('tag', $('#tag_template').html());
+    $('#tags').after(template({ tags: this.tags }));
   },
   handleCancelling(e) {
-    e.preventDefault;
-    $('#home_container').slideDown(500);
-    $('#create_contact').slideUp(500);
+    e.preventDefault();
+    $('#create_contact').slideToggle(this.ANIMATION_DELAY);
+    // $('#home_container').slideToggle(this.ANIMATION_DELAY);
+  },
+  handleAddingContact(e) {
+    e.preventDefault();
   },
   bindEvents() {
-    $('.add_contact').on('click', this.handleContactCreation.bind(this));
+    $('.add_contact').on('click', this.handleForm.bind(this));
+    $('#tags').on('focus', this.handleTagging.bind(this));
     $('#cancel').on('click', this.handleCancelling.bind(this));
+    $('#create_contact').on('submit', this.handleAddingContact.bind(this));
   },
   init() {
-    $('#create_contact').hide();
     this.bindEvents();
   },
 };
